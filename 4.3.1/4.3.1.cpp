81d79b3bf60d205fb85e53960a3095ac979dff45
ifiamawood <821371350@qq.com>
2020-04-03 13:54:41 +0800

    1

--- 4.3.1/4.3.1.cpp
+++ 4.3.1/4.3.1.cpp
@@ -2,10 +2,21 @@
 //
 
 #include "stdafx.h"
+#include<opencv.hpp>
+#include<iostream>
 
-
+using namespace cv;
 int main()
-{
-    return 0;
+{ 
+	cv::Mat out;
+    cv:Mat src = imread("D:\\lena.jpg", 0);
+	if (src.empty()) return -1;
+	float angle = -10.0, scale = 1;
+	cv::Point2f center(src.cols*0.5, src.rows*0.5);
+	const cv::Mat affine_matrix = cv::getRotationMatrix2D(center, angle, scale);
+	cv::warpAffine(src, out, affine_matrix, src.size());
+	imshow("src", src);
+	imshow("out", out);
+	waitKey(0);
 }
 
